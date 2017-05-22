ab83b3a57459aa8c377a73391ce881f0e9a72dbd
rclations <contact@rclations.com>
2017-05-22 10:23:26 -0400

    dynamic share links

--- single-longform.php
+++ single-longform.php
@@ -7,17 +7,17 @@
 get_header( "longform" );
 $series_headline = get_post_meta( $post->ID, '_ctmirror_series_headline', true );
 $featured_image = get_the_post_thumbnail_url( $post->ID, "full" );
-$header_bg =  $featured_image ? ' style="background-image:url(' . $featured_image . ');"' : '';
+$header_bg = $featured_image ? ' style="background-image:url(' . $featured_image . ');"' : '';
 ?>
 <nav id="site-navigation" class="lf-sticky" itemscope itemtype="http://schema.org/Organization">
 	<div id="longform-logo">
 		<a href="//ctmirror.org"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/mirrorlogo.png" alt="CT Mirror Logo" /></a>
 	</div>
 	<div class="longform-social">
-		<a href="https://twitter.com/home?status=https://ctmirror.org/2017/01/30/a-legacy-of-debt-connecticut-standing-on-its-own-fiscal-cliff/">            
-			<img class="share-logo" src="<?php echo bloginfo('stylesheet_directory'); ?>/images/twitter.png" />        
-		</a>        
-		<a href="https://www.facebook.com/sharer/sharer.php?u=https://ctmirror.org/2017/01/30/a-legacy-of-debt-connecticut-standing-on-its-own-fiscal-cliff/">
+		<a href="https://twitter.com/home?status=<?php the_permalink(); ?>">
+			<img class="share-logo" src="<?php echo bloginfo('stylesheet_directory'); ?>/images/twitter.png" />
+		</a>
+		<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>">
 			<img class="share-logo facebook-logo" src="<?php echo bloginfo('stylesheet_directory'); ?>/images/facebook.png" />
 		</a>
 	</div>
@@ -40,10 +40,10 @@ $header_bg =  $featured_image ? ' style="background-image:url(' . $featured_imag
 		<a href="//ctmirror.org"><img src="<?php echo bloginfo('stylesheet_directory'); ?>/images/mirrorlogo.png" alt="CT Mirror Logo" /></a>
 	</div>
 	<div class="longform-social">
-		<a href="https://twitter.com/home?status=https://ctmirror.org/2017/01/30/a-legacy-of-debt-connecticut-standing-on-its-own-fiscal-cliff/">            
-			<img class="share-logo" src="<?php echo bloginfo('stylesheet_directory'); ?>/images/twitter.png" />        
-		</a>        
-		<a href="https://www.facebook.com/sharer/sharer.php?u=https://ctmirror.org/2017/01/30/a-legacy-of-debt-connecticut-standing-on-its-own-fiscal-cliff/">
+		<a href="https://twitter.com/home?status=<?php the_permalink(); ?>">
+			<img class="share-logo" src="<?php echo bloginfo('stylesheet_directory'); ?>/images/twitter.png" />
+		</a>
+		<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>">
 			<img class="share-logo facebook-logo" src="<?php echo bloginfo('stylesheet_directory'); ?>/images/facebook.png" />
 		</a>
 	</div>
